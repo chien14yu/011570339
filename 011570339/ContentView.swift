b475de4f61a485e8c2655e21c41ca1e707f29528
Gary <garyhu17@gmail.com>
2024-10-01 10:23:59 +0800

    Initial Commit

---
+++ 011570339/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  011570339
+//
+//  Created by user11 on 2024/10/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 011570339/_11570339App.swift
@@ -0,0 +1,17 @@
+//
+//  _11570339App.swift
+//  011570339
+//
+//  Created by user11 on 2024/10/1.
+//
+
+import SwiftUI
+
+@main
+struct _11570339App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
